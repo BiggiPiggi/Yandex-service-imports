f31cd7ac1cac8f293314322ca96307d8225cf86b
Aleksandr <aszmey.war@gmail.com>
2019-08-01 08:50:22 +0500

    add additional function for insert query

--- imports/models.py
+++ imports/models.py
@@ -16,7 +16,10 @@ class Citizen(models.Model):
     birth_date = models.DateField()
     gender = models.CharField(max_length=16)
 
-    relatives = models.ManyToManyField(to='self')
+    relatives = models.ManyToManyField(to='self', symmetrical=True)
+
+    def get_insert_values(self):
+        return self.import_id.import_id, self.citizen_id, self.town, self.street, self.building, self.appartement, self.name, self.birth_date.strftime('%Y-%m-%d'), self.gender
 
     class Meta:
         unique_together = (('import_id', 'citizen_id'),)
