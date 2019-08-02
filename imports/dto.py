2353fdb1e9137d0b447ddd04da4bc290721bdc4f
Aleksandr <aszmey.war@gmail.com>
2019-08-02 12:50:39 +0500

    return previous crutches of separate providing relatives

--- imports/dto.py
+++ imports/dto.py
@@ -8,7 +8,7 @@ class DataResponse:
 
 
 class CitizenDTO:
-    def __init__(self, citizen=None):
+    def __init__(self, citizen=None, relatives=None):
         self.citizen_id = citizen.citizen_id if citizen else None
         self.town = citizen.town if citizen else None
         self.street = citizen.street if citizen else None
@@ -17,7 +17,7 @@ class CitizenDTO:
         self.name = citizen.name if citizen else None
         self.birth_date = citizen.birth_date.strftime('%d.%m.%Y') if citizen else None
         self.gender = citizen.gender if citizen else None
-        self.relatives = [c.citizen_id for c in citizen.relatives.all()] if citizen else None
+        self.relatives = relatives if relatives is not None else [c.citizen_id for c in citizen.relatives.all()] if citizen else None
 
 
 class CitizenDTOEncoder(json.JSONEncoder):
